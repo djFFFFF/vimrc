f7bf8aa2c69455da258727adf721d0a4b7ce37fb
dujiangfan <dujiangfan@douban.com>
2013-10-13 17:08:00 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+rm ~/.vim/bundle -rf
+rm ~/.vimrc
+
+echo "installing Vundle"
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+ln -s ~/.vim/vimrc ~/.vimrc
+
+echo "installing plugin using Vundle"
+vim +BundleInstall! +BundleClean +q
+
+cd ~
+mkdir ycm_build
+cd ycm_build
+
+cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/cpp
+make ycm_core
+
+cd ~
+rm -rf ~/ycm_build
+
